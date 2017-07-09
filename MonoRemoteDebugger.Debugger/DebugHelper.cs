cd804f6301dd580d0448a3b2066b883aef40a859
Gordian <gordian.cs@web.de>
2017-07-09 22:59:57 +0200

    TraceEnteringMethod print only filename.

--- MonoRemoteDebugger.Debugger/DebugHelper.cs
+++ MonoRemoteDebugger.Debugger/DebugHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using NLog;
@@ -11,7 +12,7 @@ namespace MonoRemoteDebugger.Debugger
 
         internal static void TraceEnteringMethod([CallerMemberName] string callerMember = "", [CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = 0)
         {
-            logger.Trace($"Entering: {callerMember} - {callerFilePath}({callerLineNumber})");
+            logger.Trace($"Entering: {callerMember} - {Path.GetFileName(callerFilePath)}({callerLineNumber})");
 
             //MethodBase mth = new StackTrace().GetFrame(1).GetMethod();
             //if (mth.ReflectedType != null)
